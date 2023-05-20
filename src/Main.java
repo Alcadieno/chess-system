27895804adcf6c45f0ac201dd5c906352369c184
Fernando Henrique <fernandoh.class@gmail.com>
2023-05-19 21:27:37 -0300

    board

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position posi1= new Position(3,5);
-        System.out.println(posi1);
+         Board board = new Board(8,8);
     }
 }
